b38647568611e80f5bf4636dc32633e3ed42654c
Марти <@DESKTOP-A7REVJ6>
2024-04-04 20:45:01 +0300

    Добавьте файлы проекта.

---
+++ Gihab/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace git_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+    }
+}
